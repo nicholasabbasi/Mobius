e95994748427dde26cfbabe6b31c4925db74b191
chbachman <cbachman@bachmangroup.com>
2018-03-19 17:06:30 -0500

    FSM Start, haven't tested much.

---
+++ Assets/Scripts/FSM/PatrolAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Actions/Patrol")]
+public class PatrolAction : Action {
+    public override void Act(StateController controller) {
+        var nav = controller.NavMeshAgent;
+
+        var waypoint = controller.Waypoints[controller.NextWayPoint];
+        RaycastHit hit;
+
+        if (Physics.Raycast(waypoint.position, Vector3.down, out hit)) {
+            nav.destination = hit.point;
+        }
+
+        nav.isStopped = false;
+
+        if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending) {
+            controller.NextWayPoint = (controller.NextWayPoint + 1) % controller.Waypoints.Count;
+        }
+    }
+}
---
+++ Assets/Scripts/FSM/StateController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+    public class StateController : MonoBehaviour {
+    public State CurrentState;
+    public State RemainState;
+    
+    // For drawing the debugging.
+    public Transform Eyes;
+    public float Size = 1;
+    
+    public List<Transform> Waypoints;
+
+    [HideInInspector] public NavMeshAgent NavMeshAgent;
+    [HideInInspector] public int NextWayPoint;
+    [HideInInspector] public Transform ChaseTarget;
+    [HideInInspector] public float StateTimeElapsed;
+
+    private bool _active;
+
+    public bool Active {
+        get { return _active; }
+        set {
+            NavMeshAgent.enabled = value;
+            _active = value;
+        }
+    }
+
+    private void Awake() {
+        NavMeshAgent = GetComponent<NavMeshAgent>();
+        Active = true;
+    }
+
+    private void Update() {
+        if (!Active)
+            return;
+        CurrentState.UpdateState(this);
+    }
+
+    private void OnDrawGizmos() {
+        if (CurrentState != null && Eyes != null) {
+            Gizmos.color = CurrentState.SceneGizmoColor;
+            Gizmos.DrawWireSphere(Eyes.position, Size);
+        }
+    }
+
+    public void TransitionToState(State nextState) {
+        if (nextState != RemainState) {
+            CurrentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool HasTimeElapsed(float duration) {
+        StateTimeElapsed += Time.deltaTime;
+        return StateTimeElapsed >= duration;
+    }
+
+    private void OnExitState() {
+        StateTimeElapsed = 0;
+    }
+}
