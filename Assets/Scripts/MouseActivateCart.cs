f391b3f1c0d91004b8dbf4441beac3b0f31dd830
chbachman <cbachman@bachmangroup.com>
2018-04-04 13:52:30 -0500

    Fix Lever, Add Explosion Start, Remove Double Audio

--- Assets/Scripts/MouseActivateCart.cs
+++ Assets/Scripts/MouseActivateCart.cs
@@ -13,13 +13,15 @@ public class MouseActivateCart : MonoBehaviour
         {
 
             RaycastHit hit;
+            print("Found Hit!");
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f))
             {
                 if (hit.transform)
                 {
-                    if (hit.transform.gameObject.name == "VRLever")
+                    print(hit.transform.parent.gameObject.name);
+                    if (hit.transform.parent.gameObject.name == "VRLever")
                     {
 
                         BezierController.StartMove = 1;
