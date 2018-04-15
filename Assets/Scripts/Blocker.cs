c4050453a11f5ce4faec9182e410e4d617d4e60a
kwu3 <37185676+kwu3@users.noreply.github.com>
2018-04-14 21:10:46 -0500

    Added something for assignment 10
    Added a blocker for tunel entrance
    Added Unlit Shader for that block
    Add bomb event to remove blocker

---
+++ Assets/Scripts/Blocker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour {
+
+    public static bool Bombed;
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+    // Use this for initialization
+    void Start () {
+        Bombed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Bombed == true)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    if (hit.transform.gameObject.name == "Blocker")
+                    {
+                        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+
+                       // Destroy(gameObject);          //attach this to Cube and sucessfully destroy the Cube
+
+                    }
+
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+}
