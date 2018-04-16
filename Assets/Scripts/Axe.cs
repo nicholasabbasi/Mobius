23e2c701ba1ead8a94f152dad8928f96a3edca82
kwu3 <37185676+kwu3@users.noreply.github.com>
2018-04-15 22:27:25 -0500

    add more gameplay
    Added town music;
    Add another gameplay
        need to get an axe to chop the tree to spawn the cross to stop the skulleton in final room.

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    if (hit.transform.gameObject.name == "Axe")
+                    {
+                        Tree.hasAxe = true;
+                       
+
+                        DestroyObject(this.gameObject);
+
+                    }
+
+
+                }
+
+            }
+
+
+        }
+    }
+}
--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -45,8 +45,6 @@ public class Bomb : MonoBehaviour {
         GetComponent<AudioSource>().Play();
        
 
-
-
     }
 
 
